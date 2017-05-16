95e3c6c06100456fbe631f5ebcfebc01bdd7779c
Matthew <mhoward865@gmail.com>
2017-05-16 17:47:44 +0100

    commit2

---
+++ src/app/home.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    template: '<h1>Home Page</h1>'
+})
+export class HomeComponent {
+
+}
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: '<h1>NOT FOUND</h1>'
+})
+export class NotFoundComponent {
+    
+}
